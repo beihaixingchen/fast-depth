35f064c99355ac9162eec616999c888eaed21a5f
beihaixingchen <52447914+beihaixingchen@users.noreply.github.com>
2021-05-11 21:14:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ import models
 from metrics import AverageMeter, Result
 import utils
 
+from torchviz import make_dot
+
+from torchvision import datasets, transforms
+from torchvision.transforms import ToPILImage
+from PIL import Image
+import matplotlib.pyplot as plt
+import cv2
+
+import onnx
+import onnxruntime as ort
+
+
 args = utils.parse_command()
 print(args)
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu # Set the GPU.
@@ -28,6 +40,8 @@ def main():
 
     # Data loading code
     print("=> creating data loaders...")
+
+    '''
     valdir = os.path.join('..', 'data', args.data, 'val')
 
     if args.data == 'nyudepthv2':
@@ -40,6 +54,8 @@ def main():
     val_loader = torch.utils.data.DataLoader(val_dataset,
         batch_size=1, shuffle=False, num_workers=args.workers, pin_memory=True)
     print("=> data loaders created.")
+    '''
+
 
     # evaluation mode
     if args.evaluate:
@@ -47,6 +63,7 @@ def main():
         "=> no model found at '{}'".format(args.evaluate)
         print("=> loading model '{}'".format(args.evaluate))
         checkpoint = torch.load(args.evaluate)
+        # print(checkpoint)
         if type(checkpoint) is dict:
             args.start_epoch = checkpoint['epoch']
             best_result = checkpoint['best_result']
@@ -55,9 +72,74 @@ def main():
         else:
             model = checkpoint
             args.start_epoch = 0
-        output_directory = os.path.dirname(args.evaluate)
-        validate(val_loader, model, args.start_epoch, write_to_file=False)
+        #print(model)
+
+        model.eval()
+
+        #图片输入
+        filename = 'image.jpg'
+        image = Image.open(filename).convert('RGB') #读取图像，转换为三维矩阵
+        image = image.resize((224,224),Image.ANTIALIAS) #将其转换为要求的输入大小224*224
+        transform = transforms.Compose([transforms.ToTensor()])
+        img = transform(image) #转为Tensor
+        x = img.resize(1,3,224,224) #如果存在要求输入图像为4维的情况，使用resize函数增加一维
+        
+
+        #深度推理
+        #x = torch.rand(1,3,224,224)
+        x_torch = x.type(torch.cuda.FloatTensor)
+        depth = model(x_torch)
+
+
+        #图片输出
+        depth_min = depth.min()
+        depth_max = depth.max()
+        max_val = (2**(8))-1
+
+        if depth_max - depth_min > np.finfo("float").eps:
+            out = max_val * (depth - depth_min) / (depth_max - depth_min)
+        else:
+            out = np.zeros(depth.shape, dtype=depth.type)
+        
+        out = out.cpu().detach().numpy()  # tensor转化为np.array
+        out = out.reshape(224,224)  # (1,1,224,224)转为(224,224)
+        print(out)
+        out = Image.fromarray(out) 
+        out = out.convert('L')  # 这样才能转为灰度图，如果是彩色图则改L为‘RGB’
+        out.save('depth.png')
+        
+            
+        '''
+
+        #转化为onnx
+        input_names = ["input"]
+        output_names = ["output"]
+        dummy_input = torch.randn(1, 3, 224, 224, device='cuda')
+        #torch.onnx.export(model, dummy_input, "FastDepth.onnx", verbose=True, input_names=input_names, output_names=output_names)
+
+
+        #检验onnx转化是否正确
+        print('checking onnx')
+        x = x.cpu().detach().numpy()
+        model = onnx.load("FastDepth.onnx")
+        ort_session = ort.InferenceSession('FastDepth.onnx')
+        out_onnx = ort_session.run(None, {'input': x})
+        out_onnx = np.array(out_onnx).reshape(224,224)
+        
+        print(depth)
+        print(out_onnx)  
+
+        
+        #生成结构图
+        g = make_dot(y)
+        g.render('fastdepth', view=False)
+        print('ok')
+        '''
+
+        #output_directory = os.path.dirname(args.evaluate)
+        #validate(val_loader, model, args.start_epoch, write_to_file=False)
         return
+
 
 
 def validate(val_loader, model, epoch, write_to_file=True):
